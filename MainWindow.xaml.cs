b8dd7c56e68dd0173e18acb94fc371e6dceafd67
ModeusTheDemon <116836559+ModeusTheDemon@users.noreply.github.com>
2025-01-24 01:52:35 +0200

    Lab1
    Completed(probably right)

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MinHeight = 400;
+            MinWidth = 600;
+            MaxHeight = 800;
+            MaxWidth = 1000;
+            Title = "Бондаренко Ярослав lab1";
+            Background = (Brush)System.ComponentModel.TypeDescriptor.GetConverter(typeof(Brush)).ConvertFromInvariantString("Red");
+        }
+
+        public void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Background == (Brush)System.ComponentModel.TypeDescriptor.GetConverter(typeof(Brush)).ConvertFromInvariantString("Red"))
+            {
+                Background = (Brush)System.ComponentModel.TypeDescriptor.GetConverter(typeof(Brush)).ConvertFromInvariantString("Green");
+            }
+            else
+            {
+                Background = (Brush)System.ComponentModel.TypeDescriptor.GetConverter(typeof(Brush)).ConvertFromInvariantString("Red");
+            }
+        }
+
+        public void OnMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Minimized;
+            }
+            else if (WindowState == WindowState.Minimized)
+            {
+                WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                WindowState = WindowState.Normal;
+            }
+        }
+
+        public void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                var newWidth = (Width == MaxWidth) ? MinWidth : MaxWidth;
+                var newHeight = (Height == MaxHeight) ? MinHeight : MaxHeight;
+
+                Width = newWidth;
+                Height = newHeight;
+            }
+            else if (e.Key == Key.Escape)
+            {
+                Close();
+            }
+        }
+    }
+}
